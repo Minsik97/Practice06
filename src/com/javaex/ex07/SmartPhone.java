a98d0fac75c85f91934df60b1979eeb46dafc424
insta <insta@DESKTOP-IAVPL5C>
2020-12-01 21:07:50 +0900

    추가

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -4,11 +4,18 @@ public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
         
-        //코드작성
-        
+    //코드작성
+        if("앱".equals(str)) {
+        	app();
+        }else {
+        	super.execute(str);
+        }
     }
  
     //메소드작성
+    	public void app() {
+    		System.out.println("앱실행");
+    	}
     
     //메소드작성
     
