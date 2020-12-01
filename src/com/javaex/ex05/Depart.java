f5e7bc73681b0079b0eecf6a6bb95a2136a57b7e
insta <insta@DESKTOP-IAVPL5C>
2020-12-01 20:53:52 +0900

    추가

--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -1,7 +1,30 @@
 package com.javaex.ex05;
 
-public class Depart {
+public class Depart extends Employee {
+	// 필드
+	private String Depart;
 
-	 //코드작성
+	// 생성자
+	public Depart() {
+	}
+
+	public Depart(String depart, int salary, String name) {
+		super(name, salary);
+		Depart = depart;
+	}
+
+	// 메소드 g/s
+	public String getDepart() {
+		return Depart;
+	}
+
+	public void setDepart(String depart) {
+		Depart = depart;
+	}
+	
+	//메소드 일반
+	public void showInformation() {
+		System.out.println("이름:" + getName() + "    연봉:" + getSalary() + "    부서:" + Depart);
+	}
 
 }
--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -1,7 +1,17 @@
 package com.javaex.ex06;
 
-public class MyBase extends Base{
+public class MyBase extends Base {
 
-    //코드작성
-    
+	// 코드작성
+	public void service(String Base) {
+
+		if (Base.equals("오후")) {
+			System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+		} else if (Base.equals("낮")) {
+			System.out.println("낮에는 열심히 수업듣자");
+		} else if (Base.equals("밤")) {
+			System.out.println("밤에는 숙면");
+		}
+	}
+
 }
